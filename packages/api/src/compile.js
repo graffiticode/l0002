0e41ab4953afcff761c9a2ce8a51dde3ac7b8abf
Jeff Dyer <jeffdyer@acm.org>
2026-03-17 11:53:48 -0700

    Add usage guide and add new function isempty

--- packages/api/src/compile.js
+++ packages/api/src/compile.js
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 import { compiler } from './compiler.js';
 export async function compile({ auth, authToken, code, data, config }) {
-  // console.log("compile() code=" + JSON.stringify(code, null, 2));
-  // console.log("compile() data=" + JSON.stringify(data, null, 2));
+  console.log("compile() code=" + JSON.stringify(code, null, 2));
+  console.log("compile() data=" + JSON.stringify(data, null, 2));
   if (!code || !data) {
     throw new Error('Missing required parameters: code and data');
   }
